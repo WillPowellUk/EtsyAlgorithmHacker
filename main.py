faf3ce463aae4a678a2d1f4084473d5e4c266f8f
Will <me@willpowell.uk>
2023-02-22 16:23:45 +0000

    Functions work independently

--- main.py
+++ main.py
@@ -26,50 +26,63 @@ class Webscrape:
         self.driver.get(websiteURL)
         self.websiteURL = websiteURL
 
+    def findListing(self):
+        # accept cookies
+        web.driver.find_element(By.XPATH, '//*[@id="gdpr-single-choice-overlay"]/div/div[2]/div[2]/button').click() 
+
+        # search for key word
+        searchBar = web.driver.find_element(By.XPATH, '//*[@id="global-enhancements-search-query"]')
+        searchBar.click()
+        searchBar.send_keys("Grinder")
+        searchBar.send_keys(Keys.RETURN)
+
+        # filter 
+        web.driver.find_element(By.XPATH, '//*[@id="search-filter-button"]').click()
+        web.driver.get('https://www.etsy.com/search?q=grinder&explicit=1&min=7.99&max=7.99&ship_to=GB')
+
+        # wait for page to load, then select listing containing 'Biscuit Herb'
+        web.driver.find_element(By.XPATH, "//*[contains(text(), 'Biscuit Herb')]").click()
+
+        return web.driver.current_url
+
+
+    def loveListing(self, url):
+        web.driver.get(url)
+
+        # accept cookies
+        web.driver.find_element(By.XPATH, '//*[@id="gdpr-single-choice-overlay"]/div/div[2]/div[2]/button').click() 
+        
+        # add to favourites using listing id 1401070274
+        web.driver.find_element(By.CSS_SELECTOR, "button[data-listing-id='1401070274']").click()
+            
+        # wait for the dropdown to become visible
+        dropdown = WebDriverWait(web.driver, 10).until(EC.visibility_of_element_located((By.ID, "variation-selector-0")))
+
+        # create a Select object for the dropdown
+        select = Select(dropdown)
+
+        # select an option by its text
+        select.select_by_visible_text("Black")
+
+        # select colour (option is black (3247764331))
+        web.driver.find_element(By.ID, "variation-selector-0").click()
+        web.driver.find_element(By.CSS_SELECTOR, "option[value='3247764331']").click()
+
+        # add to basket
+        WebDriverWait(web.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div[data-selector="add-to-cart-button"] button[type="submit"]'))).click()
+
+
     def rotateProxy(self):
         pass
-
-    def checkPage(self, keyword):
-        self.driver.get(self.websiteURL)
-        sourceText = self.driver.page_source
-        return (keyword.lower() in sourceText.lower())
 
 if __name__=="__main__":
 
     # create new driver
     web = Webscrape('https://www.etsy.com/uk/','debug')
 
-    # accept cookies
-    web.driver.find_element(By.XPATH, '//*[@id="gdpr-single-choice-overlay"]/div/div[2]/div[2]/button').click() 
-
-    # search for key word
-    searchBar = web.driver.find_element(By.XPATH, '//*[@id="global-enhancements-search-query"]')
-    searchBar.click()
-    searchBar.send_keys("Grinder")
-    searchBar.send_keys(Keys.RETURN)
-
-    # filter 
-    web.driver.find_element(By.XPATH, '//*[@id="search-filter-button"]').click()
-    web.driver.get('https://www.etsy.com/search?q=grinder&explicit=1&min=7.99&max=7.99&ship_to=GB')
+    # url = web.findListing()
     
-    # wait for page to load, then select listing containing 'Biscuit Herb'
-    web.driver.find_element(By.XPATH, "//*[contains(text(), 'Biscuit Herb')]").click()
-    
-    # add to favourites using listing id 1401070274
-    # web.driver.find_element(By.CSS_SELECTOR, "button[data-listing-id='1401070274']").click()
-        
-    # wait for the dropdown to become visible
-    # dropdown = WebDriverWait(web.driver, 10).until(EC.visibility_of_element_located((By.ID, "variation-selector-0")))
+    web.loveListing('https://www.etsy.com/uk/listing/1401070274/biscuit-herb-tobacco-grinder?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=grinder&ref=sr_gallery-1-8&frs=1&organic_search_click=1')
 
-    # create a Select object for the dropdown
-    # select = Select(dropdown)
 
-    # select an option by its text
-    # select.select_by_visible_text("Black")
 
-    # select colour (option is black (3247764331))
-    # web.driver.find_element(By.ID, "variation-selector-0").click()
-    # web.driver.find_element(By.CSS_SELECTOR, "option[value='3247764331']").click()
-
-    # add to basket
-    WebDriverWait(web.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div[data-selector="add-to-cart-button"] button[type="submit"]'))).click()
